8d333d0770576a52ce2f72d9548d026af6cdee05
Serhii Vrublevskyi <sergey.vrublevskiy@gmail.com>
2021-03-22 13:18:26 +0200

    Math class adding

---
+++ HomeWork/MathOperations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork
+{
+    class MathOperations
+    {
+        public int Sum(int numberOne, int numberTwo)
+        {
+            return numberOne + numberTwo;
+        }
+
+        public double squareCulculation(int a, int b)
+        {
+            if(a > b)
+            {
+                return Math.Sqrt(a); 
+            }
+            else
+            {
+                return Math.Sqrt(b);
+            }
+        }
+    }
+}
